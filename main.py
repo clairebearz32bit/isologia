89aa6af244f092b03f63a611583f4d51b4cc86af
clairebearz32bit <clairekingthequeen@gmail.com>
2026-09-09 17:30:15 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import json
+
+from src.word import Word
+
+if __name__ == '__main__':
+    word = Word("etymology")
---
+++ src/word.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class Word:
+    def __init__(self, word):
+        URL = "https://en.wiktionary.org/w/api.php?action=parse&page={}&prop=wikitext&format=json&origin=*"
+        headers = {"User-Agent": "Isologia/0.0 (https://github.com/clairebearz32bit/isologia)"}
+
+        response = requests.get(URL.format(word), headers=headers).json()["parse"]["wikitext"]["*"]
+        print(response)
+        if "===Etymology===" not in response:
+            # Maybe add another source here as a fallback?
+            raise Exception("This word does not have an etymology section on Wiktionary.")
+
+        self.etymology = response.split("===Etymology===")[1].split("From")[1].split("\n")[0]
+        print(self.etymology)

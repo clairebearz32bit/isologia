4b8052677766d9fcf727dea9f1526bc2e9b792dd
clairebearz32bit <clairekingthequeen@gmail.com>
2026-09-12 16:46:46 -0700

    Remove unrelated IDE files

--- src/word.py
+++ src/word.py
@@ -6,11 +6,13 @@ class Word:
         URL = "https://en.wiktionary.org/w/api.php?action=parse&page={}&prop=wikitext&format=json&origin=*"
         headers = {"User-Agent": "Isologia/0.0 (https://github.com/clairebearz32bit/isologia)"}
 
-        response = requests.get(URL.format(word), headers=headers).json()["parse"]["wikitext"]["*"]
-        print(response)
+        response = requests.get(URL.format(word), headers=headers).json()
+
         if "===Etymology===" not in response:
             # Maybe add another source here as a fallback?
             raise Exception("This word does not have an etymology section on Wiktionary.")
 
-        self.etymology = response.split("===Etymology===")[1].split("From")[1].split("\n")[0]
+        self.etymology = response
         print(self.etymology)
+
+    def parse_response(self):

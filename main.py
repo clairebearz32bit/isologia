602a135c139b65abd964bfc4bf43b38049c346d8
clairebearz32bit <clairekingthequeen@gmail.com>
2026-09-13 01:04:10 -0700

    Too tired to write a comprehensive message, will amend later

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from src.word import Word
 
 if __name__ == '__main__':
     word = Word("etymology")
-    r = word.get_etymology()
-    print(r)
+    # r = word.get_etymology()
+    print(word.parse_etymology())
--- src/word.py
+++ src/word.py
@@ -3,6 +3,7 @@ import json
 import requests
 import networkx as nx
 
+from dataclasses import dataclass
 
 class Word:
     def __init__(self, word):
@@ -32,28 +33,36 @@ class Word:
             response,
             flags=re.MULTILINE | re.DOTALL,
         )
+        if not match:
+            return None
 
-        t = str.translate("{}", "\n", )
-        etymology_text = match.group("etymology").strip().translate(t) if match else None
+        etymology = match.group("etymology").strip()
 
         if dump_json:
             filepath = "./{}_ETYMOLOGY.json".format(self.word)
             with open(filepath, "w+") as fp:
-                json.dump(etymology_text, fp, indent=4)
+                json.dump(etymology, fp, indent=4)
 
-        return etymology_text
+        return etymology
 
     # Just uses static text for now to avoid API calls while I write the logic.
     def parse_etymology(self):
         # r = self.get_response()
-        r = """
-        {{etymon|en|:ubor|fr:croissant|tree=1}}
-        {{root|en|ine-pro|*ḱer- (grow)}}
-        {{ubor|en|fr|croissant||crescent}}, present participle of {{m|fr|croître||to grow}}. {{doublet|en|crescent}}.
-        """
+        r = """{{etymon|en|:inh|enm:ethymologie|tree=1}}\nFrom {{inh|en|enm|ethymologie}}, from {{der|en|fro|ethimologie}}, from {{der|en|la|etymologia}}, from {{der|en|grc|\u1f10\u03c4\u03c5\u03bc\u03bf\u03bb\u03bf\u03b3\u03af\u03b1}}, from {{m|grc|\u1f14\u03c4\u03c5\u03bc\u03bf\u03bd||true sense}} and {{m|grc|-\u03bb\u03bf\u03b3\u03af\u03b1||study or logic of}}, from {{m|grc|\u03bb\u03cc\u03b3\u03bf\u03c2||word; explanation}}. {{surf|en|etymo-|-logy}}."""
+        r = re.split(r"[{}\n]", r)
+        r[:] = [s for s in r if s not in ("")]
+
+        print(r)
+
         nodes = []
 
         return nodes
 
     def create_graph(self, word):
         G = nx.MultiDiGraph()
+
+
+@dataclass
+class Node:
+    word: str
+    depth: int
